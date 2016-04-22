99aac5aae228c25c2e6148f696a68de848749052
mac.grewal <mac.grewal@digital.hmrc.gov.uk>
2016-04-22 15:18:54 +0100

    added simple play application with hello world page

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
-
 # pensions-lifetime-allowance-frontend
 
 [![Build Status](https://travis-ci.org/hmrc/pensions-lifetime-allowance-frontend.svg?branch=master)](https://travis-ci.org/hmrc/pensions-lifetime-allowance-frontend) [ ![Download](https://api.bintray.com/packages/hmrc/releases/pensions-lifetime-allowance-frontend/images/download.svg) ](https://bintray.com/hmrc/releases/pensions-lifetime-allowance-frontend/_latestVersion)
 
 This is a placeholder README.md for a new repository
 
+### Pensions Lifetime Allowance Frontend
+
+This is the repository for the pensions lifetime allowance frontend. This provides clients with a way to view and manage their pension protections.
+
+To run the project clone the repository and in terminal $sbt "run $PORT"
+
 ### License
 
-This code is open source software licensed under the [Apache 2.0 License]("http://www.apache.org/licenses/LICENSE-2.0.html").
-    
+This code is open source software licensed under the [Apache 2.0 License]("http://www.apache.org/licenses/LICENSE-2.0.html")
---
+++ project/FrontendBuild.scala
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import sbt._
+import uk.gov.hmrc.SbtAutoBuildPlugin
+import uk.gov.hmrc.sbtdistributables.SbtDistributablesPlugin
+import uk.gov.hmrc.versioning.SbtGitVersioning
+
+object FrontendBuild extends Build with MicroService {
+
+  val appName = "pensions-lifetime-allowance-frontend"
+
+  override lazy val plugins: Seq[Plugins] = Seq(
+    SbtAutoBuildPlugin, SbtGitVersioning, SbtDistributablesPlugin
+  )
+
+  override lazy val appDependencies: Seq[ModuleID] = AppDependencies()
+}
+
+private object AppDependencies {
+  import play.PlayImport._
+  import play.core.PlayVersion
+
+  private val playHealthVersion = "1.1.0"
+  private val playJsonLoggerVersion = "2.1.1"
+  private val frontendBootstrapVersion = "6.4.0"
+  private val govukTemplateVersion = "4.0.0"
+  private val playUiVersion = "4.11.0"
+  private val playPartialsVersion = "4.2.0"
+  private val playAuthorisedFrontendVersion = "5.0.0"
+  private val playConfigVersion = "2.0.1"
+  private val hmrcTestVersion = "1.6.0"
+
+  val compile = Seq(
+    ws,
+    "uk.gov.hmrc" %% "frontend-bootstrap" % frontendBootstrapVersion,
+    "uk.gov.hmrc" %% "play-partials" % playPartialsVersion,
+    "uk.gov.hmrc" %% "play-authorised-frontend" % playAuthorisedFrontendVersion,
+    "uk.gov.hmrc" %% "play-config" % playConfigVersion,
+    "uk.gov.hmrc" %% "play-json-logger" % playJsonLoggerVersion,
+    "uk.gov.hmrc" %% "govuk-template" % govukTemplateVersion,
+    "uk.gov.hmrc" %% "play-health" % playHealthVersion,
+    "uk.gov.hmrc" %% "play-ui" % playUiVersion
+  )
+
+  trait TestDependencies {
+    lazy val scope: String = "test"
+    lazy val test : Seq[ModuleID] = ???
+  }
+
+  object Test {
+    def apply() = new TestDependencies {
+      override lazy val test = Seq(
+        "uk.gov.hmrc" %% "hmrctest" % hmrcTestVersion % scope,
+        "org.scalatest" %% "scalatest" % "2.2.6" % scope,
+        "org.pegdown" % "pegdown" % "1.5.0" % scope,
+        "org.jsoup" % "jsoup" % "1.7.3" % scope,
+        "com.typesafe.play" %% "play-test" % PlayVersion.current % scope
+      )
+    }.test
+  }
+
+  def apply() = compile ++ Test()
+}
+
+
