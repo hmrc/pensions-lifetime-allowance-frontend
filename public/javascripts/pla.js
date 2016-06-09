9cffc6c857835f823b461450706e3fd6b451af61
thomas.stacey <thomas.stacey@digital.hmrc.gov.uk>
2016-06-09 14:25:19 +0100

    Update hidden input javascript for correct yes/no values

--- public/javascripts/pla.js
+++ public/javascripts/pla.js
@@ -6,7 +6,7 @@ $(document).ready($(function() {
         var $hidden = $('#hidden')
         var $input = $self.find('input');
 
-        if ($input.val() === 'Yes' && $input.prop('checked')) {
+        if ($input.val() === 'yes' && $input.prop('checked')) {
             $hidden.show();
         } else {
             $hidden.hide();
@@ -16,9 +16,9 @@ $(document).ready($(function() {
 
             var $this = $(this);
 
-            if ($this.val() === 'Yes') {
+            if ($this.val() === 'yes') {
                 $hidden.show();
-            } else if($this.val() === 'No') {
+            } else if($this.val() === 'no') {
                 $hidden.hide();
             }
         });
