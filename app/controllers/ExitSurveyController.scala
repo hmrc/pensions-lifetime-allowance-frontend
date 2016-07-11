3314269a432723e3f4004f613dd5d17c315bd034
samuel.raspin <samuel.raspin@digital.hmrc.gov.uk>
2016-07-11 17:03:50 +0100

    exit survey form, model, controller and almost page completed

---
+++ app/controllers/ExitSurveyController.scala
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import java.util.UUID
+import play.api.mvc._
+import uk.gov.hmrc.play.frontend.controller.FrontendController
+import uk.gov.hmrc.play.http.SessionKeys
+import uk.gov.hmrc.play.http.logging.SessionId
+import scala.concurrent.Future
+import forms.ExitSurveyForm.exitSurveyForm
+import models._
+
+import views.html._
+
+object ExitSurveyController extends ExitSurveyController {
+     
+}
+
+trait ExitSurveyController extends FrontendController {
+
+    val exitSurvey = Action.async { implicit request =>
+        if (request.session.get(SessionKeys.sessionId).isEmpty) {
+            Future.successful(Ok(pages.exitSurvey.exitSurvey(exitSurveyForm)))
+        } else {
+            Future.successful(Ok(pages.exitSurvey.exitSurvey(exitSurveyForm)))
+        }
+    }
+}
