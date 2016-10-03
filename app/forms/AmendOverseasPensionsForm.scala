fd9cd65aa20e1b8026514ed1adc7bd90f9bce188
samuel-raspin <samuel.raspin@digital.hmrc.gov.uk>
2016-10-03 14:29:42 +0100

    overseas ip16 amends

---
+++ app/forms/AmendOverseasPensionsForm.scala
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package forms
+
+import common.Validation._
+import models.amendModels.AmendOverseasPensionsModel
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.i18n.Messages
+import utils.Constants
+
+object AmendOverseasPensionsForm {
+
+  val amendOverseasPensionsForm = Form (
+    mapping(
+      "amendedOverseasPensions" -> nonEmptyText,
+      "amendedOverseasPensionsAmt" -> optional(bigDecimal),
+      "protectionType" -> text,
+      "status" -> text
+    )(AmendOverseasPensionsModel.apply)(AmendOverseasPensionsModel.unapply)
+  )
+
+  def validateForm(form: Form[AmendOverseasPensionsModel]): Form[AmendOverseasPensionsModel] = {
+    if(!validationNeeded(form)) form else {
+      if(!validateFieldCompleted(form)) form.withError("amendedOverseasPensionsAmt", Messages("pla.overseasPensions.errorQuestion"))
+      else if(!validateMinimum(form)) form.withError("amendedOverseasPensionsAmt", Messages("pla.overseasPensions.errorNegative"))
+      else if(!validateMaximum(form)) form.withError("amendedOverseasPensionsAmt", Messages("pla.overseasPensions.errorMaximum"))
+      else if(!validateTwoDec(form)) form.withError("amendedOverseasPensionsAmt", Messages("pla.overseasPensions.errorDecimalPlaces"))
+      else form
+    }
+  }
+
+  private def validationNeeded(data: Form[AmendOverseasPensionsModel]): Boolean = {
+    data("amendedOverseasPensions").value.get match {
+      case "yes" => true
+      case "no" => false
+    }
+  }
+
+  private def validateFieldCompleted(data: Form[AmendOverseasPensionsModel]) = data("amendedOverseasPensionsAmt").value.isDefined
+
+  private def validateMinimum(data: Form[AmendOverseasPensionsModel]) = isPositive(data("amendedOverseasPensionsAmt").value.getOrElse("0").toDouble)
+
+  private def validateMaximum(data: Form[AmendOverseasPensionsModel]) = isLessThanDouble(data("amendedOverseasPensionsAmt").value.getOrElse("0").toDouble, Constants.npsMaxCurrency)
+
+  private def validateTwoDec(data: Form[AmendOverseasPensionsModel]) = isMaxTwoDecimalPlaces(data("amendedOverseasPensionsAmt").value.getOrElse("0").toDouble)
+
+}
