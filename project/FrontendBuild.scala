1ab702d19044177436ec54de10a91f19855cc0c7
Mark Kelly <mark.kelly@digital.hmrc.gov.uk>
2016-11-14 16:50:50 +0000

    upgraded SBT, updated MicroService and FrontendBuild

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -15,23 +15,20 @@
  */
 
 import sbt._
-import uk.gov.hmrc.SbtAutoBuildPlugin
-import uk.gov.hmrc.sbtdistributables.SbtDistributablesPlugin
-import uk.gov.hmrc.versioning.SbtGitVersioning
 
 object FrontendBuild extends Build with MicroService {
 
   val appName = "pensions-lifetime-allowance-frontend"
 
-  override lazy val plugins: Seq[Plugins] = Seq(
-    SbtAutoBuildPlugin, SbtGitVersioning, SbtDistributablesPlugin
-  )
+//  override lazy val plugins: Seq[Plugins] = Seq(
+//    SbtAutoBuildPlugin, SbtGitVersioning, SbtDistributablesPlugin
+//  )
 
   override lazy val appDependencies: Seq[ModuleID] = AppDependencies()
 }
 
 private object AppDependencies {
-  import play.PlayImport._
+  import play.sbt.PlayImport._
   import play.core.PlayVersion
 
   private val playHealthVersion = "1.1.0"
@@ -40,9 +37,11 @@ private object AppDependencies {
   private val govukTemplateVersion = "4.0.0"
   private val playUiVersion = "4.16.0"
   private val playPartialsVersion = "4.6.0"
-  private val playAuthorisedFrontendVersion = "5.7.0"
+  private val playAuthorisedFrontendVersion = "5.8.0"
   private val playConfigVersion = "2.1.0"
-  private val hmrcTestVersion = "1.8.0"
+  private val hmrcTestVersion = "1.9.0"
+  private val scalaTestVersion = "2.2.6"
+  private val pegdownVersion = "1.6.0"
   private val cachingClientVersion = "5.6.0"
   private val mongoCachingVersion = "3.2.0"
 
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -6,9 +6,9 @@ addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "1.0.0")
 
 addSbtPlugin("uk.gov.hmrc" % "sbt-git-versioning" % "0.8.0")
 
-addSbtPlugin("uk.gov.hmrc" % "sbt-distributables" % "0.10.0")
+addSbtPlugin("uk.gov.hmrc" % "sbt-distributables" % "1.0.0")
 
-addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.3.10")
+addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.5.8")
 
 resolvers += Resolver.url("scoverage-bintray", url("https://dl.bintray.com/sksamuel/sbt-plugins/"))(Resolver.ivyStylePatterns)
 
