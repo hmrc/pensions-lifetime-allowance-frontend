1228c1defbe816f819200c456926c9ae89f6e9c3
Mark Kelly <mark.kelly@digital.hmrc.gov.uk>
2017-01-11 16:24:44 +0000

    Update date in headers

--- app/enums/ApplicationOutcome.scala
+++ app/enums/ApplicationOutcome.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 HM Revenue & Customs
+ * Copyright 2017 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,4 +22,3 @@ object ApplicationOutcome extends Enumeration {
   val Rejected = Value
   val MCNeeded = Value
 }
-
