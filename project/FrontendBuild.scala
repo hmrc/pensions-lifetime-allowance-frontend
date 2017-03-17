eb1396288e2b4c41daa2fbedc7bcb523a9e03637
matthew.johnson <matthew.johnson@digital.hmrc.gov.uk>
2017-03-17 16:14:01 +0000

    DDCNLS-769 upgraded frontendbootstrap to 7.17

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -29,7 +29,7 @@ private object AppDependencies {
 
   private val playHealthVersion = "2.1.0"
   private val loggerVersion = "3.1.0"
-  private val frontendBootstrapVersion = "7.14.0"
+  private val frontendBootstrapVersion = "7.17.0"
   private val govukTemplateVersion = "5.1.0"
   private val playUiVersion = "7.0.0"
   private val playPartialsVersion = "5.3.0"
