557d415f6eecf4c494c2c4f63e1a6346dbcff0e4
Matt Roberts <matt.roberts2@digital.hmrc.gov.uk>
2017-06-02 09:30:08 +0100

    split the single entry form into two steps, still need to work out the back/start-over logic PLA-1391

--- app/controllers/LookupController.scala
+++ app/controllers/LookupController.scala
@@ -18,12 +18,15 @@ package controllers
 
 import connectors.{KeyStoreConnector, PLAConnector}
 import forms.PSALookupRequestForm.pSALookupRequestForm
-import models.{PSALookupRequest, PSALookupResult}
+import forms.PSALookupSchemeAdministratorReferenceForm.pSALookupSchemeAdministratorReferenceForm
+import forms.PSALookupProtectionNotificationNoForm.pSALookupProtectionNotificationNoForm
+import models.{PSALookupRequest, PSALookupResult, PSALookupSchemeAdministratorReferenceRequest}
 import play.api.Play.current
 import play.api.data.FormError
 import play.api.i18n.Messages.Implicits._
 import play.api.libs.json.Json
 import play.api.mvc._
+import play.api.Logger
 import uk.gov.hmrc.play.frontend.controller.FrontendController
 import uk.gov.hmrc.play.http.Upstream4xxResponse
 import utils.ActionWithSessionId
@@ -46,6 +49,60 @@ trait LookupController extends FrontendController {
   def displayLookupForm: Action[AnyContent] = ActionWithSessionId.async { implicit request =>
     Future.successful(Ok(pages.lookup.psa_lookup_form(pSALookupRequestForm)))
   }
+
+  def displaySchemeAdministratorReferenceForm: Action[AnyContent] = ActionWithSessionId.async { implicit request =>
+    keyStoreConnector.fetchAndGetFormData[PSALookupSchemeAdministratorReferenceRequest]("pensionSchemeAdministratorCheckReference").flatMap {
+      case Some(stored) =>
+        Future.successful(Ok(pages.lookup.psa_lookup_scheme_admin_ref_form(pSALookupSchemeAdministratorReferenceForm.fill(stored))))
+      case _ =>
+        Future.successful(Ok(pages.lookup.psa_lookup_scheme_admin_ref_form(pSALookupSchemeAdministratorReferenceForm)))
+    }
+  }
+
+  def submitSchemeAdministratorReferenceForm: Action[AnyContent] = ActionWithSessionId.async { implicit request =>
+    pSALookupSchemeAdministratorReferenceForm.bindFromRequest().fold(
+      formWithErrors => Future.successful(BadRequest(content = pages.lookup.psa_lookup_scheme_admin_ref_form(formWithErrors))),
+      validFormData => {
+        keyStoreConnector.saveFormData[PSALookupSchemeAdministratorReferenceRequest]("pensionSchemeAdministratorCheckReference", validFormData).map {
+          _ => Redirect(routes.LookupController.displayProtectionNotificationNoForm())
+        }
+      }
+    )
+  }
+
+  def displayProtectionNotificationNoForm: Action[AnyContent] = ActionWithSessionId.async { implicit request =>
+    Future.successful(Ok(pages.lookup.psa_lookup_protection_notification_no_form(pSALookupProtectionNotificationNoForm)))
+  }
+
+  def submitProtectionNotificationNoForm: Action[AnyContent] = ActionWithSessionId.async { implicit request =>
+    pSALookupProtectionNotificationNoForm.bindFromRequest().fold(
+      formWithErrors => Future.successful(BadRequest(content = pages.lookup.psa_lookup_protection_notification_no_form(formWithErrors))),
+      validFormData => {
+        keyStoreConnector.fetchAndGetFormData[PSALookupSchemeAdministratorReferenceRequest]("pensionSchemeAdministratorCheckReference").flatMap {
+          case Some(stored) =>
+            plaConnector.psaLookup(stored.pensionSchemeAdministratorCheckReference, validFormData.lifetimeAllowanceReference).flatMap {
+              result =>
+                result.status match {
+                  case OK =>
+                    val resultData = Json.fromJson[PSALookupResult](result.json).get
+                    Logger.debug("MJR resultData = " + resultData)
+                    if (resultData.psaCheckResult == 0) Future.successful(BadRequest(pages.lookup.psa_lookup_form(notFoundLookupForm)))
+                    else keyStoreConnector.saveFormData[PSALookupResult]("psa-lookup-result", resultData).map {
+                      _ => Redirect(routes.LookupController.displayLookupResults())
+                    }
+                  case _ => throw new RuntimeException("unable to get a lookup from the plaConnector")
+                }
+            }.recover {
+              case r: Upstream4xxResponse if r.upstreamResponseCode == NOT_FOUND => BadRequest(pages.lookup.psa_lookup_form(notFoundLookupForm))
+            }
+          case _ =>
+            throw new RuntimeException("unable to get the pensionSchemeAdministratorCheckReference from the keyStoreConnector")
+        }
+      // TODO look at using for comprehension here
+      }
+    )
+  }
+
 
   def submitLookupRequest: Action[AnyContent] = ActionWithSessionId.async { implicit request =>
     pSALookupRequestForm.bindFromRequest().fold(
@@ -71,7 +128,7 @@ trait LookupController extends FrontendController {
   def displayLookupResults: Action[AnyContent] = ActionWithSessionId.async { implicit request =>
     keyStoreConnector.fetchAndGetFormData[PSALookupResult]("psa-lookup-result").map {
       case Some(result) => Ok(pages.lookup.psa_lookup_results(result))
-      case None => Redirect(routes.LookupController.displayLookupForm())
+      case None => Redirect(routes.LookupController.displaySchemeAdministratorReferenceForm())
     }
   }
 
---
+++ app/forms/PSALookupProtectionNotificationNoForm.scala
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package forms
+
+import models.PSALookupProtectionNotificationNoRequest
+import play.api.Play.current
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.data.validation.{Constraint, Invalid, Valid, ValidationError}
+import play.api.i18n.Messages
+import play.api.i18n.Messages.Implicits._
+
+object PSALookupProtectionNotificationNoForm {
+
+  def pSALookupProtectionNotificationNoForm: Form[PSALookupProtectionNotificationNoRequest] = {
+    Form(mapping(
+      "lifetimeAllowanceReference" -> text.verifying(ltaRefConstraint)
+    )(PSALookupProtectionNotificationNoRequest.apply)(PSALookupProtectionNotificationNoRequest.unapply))
+  }
+
+  private val npsRefRegex = """^(?i)(IP14|IP16|FP16)[0-9]{10}[ABCDEFGHJKLMNPRSTXYZ]$""".r
+  private val tpssRefRegex = """^[1-9A][0-9]{6}[ABCDEFHXJKLMNYPQRSTZW]$""".r
+
+
+  val ltaRefConstraint: Constraint[String] = Constraint("constraints.ltarefcheck")({
+    ltaRef =>
+      val errors = ltaRef match {
+        case "" => Seq(ValidationError(Messages("psa.lookup.form.ltaref.required")))
+        case npsRefRegex(_) => Nil
+        case tpssRefRegex() => Nil
+        case _ => Seq(ValidationError(Messages("psa.lookup.form.ltaref.invalid")))
+      }
+      if (errors.isEmpty) Valid else Invalid(errors)
+  })
+
+}
