f41fe106e6b6be1116bc8a2464da1a0ce3726bb7
nehakannaujia1 <50657531+nehakannaujia1@users.noreply.github.com>
2025-04-10 15:12:23 +0100

    fix for route compilation error

--- test/controllers/redirect/RedirectControllerSpec.scala
+++ test/controllers/redirect/RedirectControllerSpec.scala
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package controllers
+package controllers.redirect
 
 import org.scalatestplus.mockito.MockitoSugar
 import play.api.mvc.MessagesControllerComponents
