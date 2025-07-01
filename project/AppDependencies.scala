1215197cb913a863e99e157c6d5f0989dadabc04
Eoghan-Spillane-Accenture <201935567+Eoghan-Spillane-Accenture@users.noreply.github.com>
2025-07-01 09:57:01 +0100

    DL-16805: Gov UK Rebrand 2025

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,7 @@ object AppDependencies {
   import play.sbt.PlayImport.*
 
   private val bootstrapVersion                  = "9.13.0"
-  private val playFrontendVersion               = "12.6.0"
+  private val playFrontendVersion               = "12.7.0"
   private val mongoPlayVersion                  = "2.6.0"
   private val pekkoVersion                      = "1.0.2"
   private val playConditionalFormMappingVersion = "3.3.0"
