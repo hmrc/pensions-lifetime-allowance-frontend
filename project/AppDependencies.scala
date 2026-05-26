3f5d2c1affd7269a171e77abd41e2a2da4e506e2
Eoghan-Spillane-Accenture <201935567+Eoghan-Spillane-Accenture@users.noreply.github.com>
2026-05-26 09:46:19 +0100

    DL-19195: Split pages into Authenticated and Unauthenticated layouts

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,6 @@ object AppDependencies {
   import play.sbt.PlayImport.*
 
   private val bootstrapVersion                  = "10.7.0"
-  private val playFrontendVersion               = "13.7.0"
   private val mongoPlayVersion                  = "2.12.0"
   private val pekkoVersion                      = "1.5.0"
   private val playConditionalFormMappingVersion = "3.5.0"
@@ -14,9 +13,9 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % playFrontendVersion,
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % playConditionalFormMappingVersion,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % mongoPlayVersion,
+    "uk.gov.hmrc"       %% "sca-wrapper-play-30"                   % "5.0.0",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % mongoPlayVersion
   )
 
   val testDependencies: Seq[ModuleID] = Seq(
