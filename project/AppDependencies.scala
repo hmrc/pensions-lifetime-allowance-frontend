a51996f84bf33ed9790060315b5da3bd45ae6a6c
nehakannaujia1 <50657531+nehakannaujia1@users.noreply.github.com>
2026-07-13 14:31:15 +0100

    FIxed pages for 2016

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,7 @@ object AppDependencies {
   import play.sbt.PlayImport.*
 
   private val bootstrapVersion                  = "10.7.0"
-  private val playFrontendVersion               = "13.7.0"
+  private val playFrontendVersion               = "13.9.0"
   private val mongoPlayVersion                  = "2.12.0"
   private val pekkoVersion                      = "1.6.0"
   private val playConditionalFormMappingVersion = "3.5.0"
